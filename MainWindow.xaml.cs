bace1e97e018f13c38aeb5ac989f3d3b022b2ae6
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2025-01-29 15:28:33 -0500

    Fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,19 +11,65 @@ using System.Windows.Shapes;
 
 namespace Graphote
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+    FiguraTridimensional? FiguraSeleccionada = null;
+    public MainWindow()
         {
             InitializeComponent();
         }
 
+    private void lbl_seleccionar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      if(FiguraSeleccionada != null) 
+        grid_transofrmaciones.Visibility = Visibility.Visible;
+      else
+        grid_transofrmaciones.Visibility = Visibility.Hidden;
+    }
+
+    private void lbl_seleccionar_reiniciar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+
+    private void lbl_seleccionar_salir_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      this.Close();
+    }
+
     private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
     {
 
-        }
     }
+
+    private void slider_rotarX_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
+
+    private void slider_rotarY_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
+
+    private void slider_rotarZ_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
+
+    private void btn_focusZ_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+
+    private void btn_focusX_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+
+    private void btn_focusY_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+
+    }
+  }
 }
