31d3658377478abefbf9407e0c9aff5760b0b399
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2025-01-29 12:23:38 -0500

    Initial commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +19,11 @@ namespace Graphote
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+    private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
         }
     }
 }
