3522ae4ae2a2b852f12450dde48edfc2898e17d6
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2025-01-30 09:15:00 -0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -194,6 +194,7 @@ namespace Graphote
     private void lbl_añadir_MouseDown(object sender, MouseButtonEventArgs e)
     {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       if (grid_figuras.Visibility == Visibility.Visible)
       {
         lbl_añadir.Background = null;
@@ -207,6 +208,8 @@ namespace Graphote
                   (Color)ColorConverter.ConvertFromString("#457B9D"));
       }
 =======
+=======
+>>>>>>> Stashed changes
         if (grid_figuras.Visibility == Visibility.Visible)
         {
             lbl_añadir.Background = null;
